8ec949603c2531b5d8654db5c26a319709ca2c98
JoCh <johancz@users.noreply.github.com>
2021-03-03 13:56:05 +0100

    After meeting with Stephan

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,5 +1,57 @@
 using System.Collections.Generic;
 
+/*---------------------------- Efter 2021-03-03: ----------------------------*/
+public interface IMilk
+{
+    IMilk AddMilk(int amount);
+}
+
+public interface ICoffeeSyrup
+{
+    ICoffeeSyrup AddCoffeeSyrup(int amount);
+}
+
+interface IEspressoMachine
+{
+    IEspressoMachine AddWater(int amount);
+    IEspressoMachine AddBeans(int amount);
+    Beverage ToBeverage();
+}
+
+class EspressoMachine : IEspressoMachine
+{
+    List<Ingredient> Ingredients { get; }
+
+    public IEspressoMachine AddWater(int amount)
+    {
+        Ingredients.Add(new Ingredient()
+        {
+            Name = "Water",
+            Amount = amount
+        });
+
+        return this;
+    }
+
+    public IEspressoMachine AddBeans(int amount)
+    {
+        Ingredients.Add(new Ingredient()
+        {
+            Name = "Bean",
+            Amount = amount
+        });
+
+
+        return this;
+    }
+
+    public Beverage ToBeverage()
+    {
+        return new Espresso();
+    }
+}
+
+
 public interface IBeverage
 {
     //private List<Ingredient> Ingredients => _ingredient;
@@ -7,26 +59,7 @@ public interface IBeverage
     List<Ingredient> Ingredients { get; }
     string CupType { get; }
 
-    IBeverage AddWater(int amount);
-    IBeverage AddBeans(int amount);
-    IBeverage AddMilk(int amount);
 }
-
-
-//enum CoffeSorts
-//{
-//    Robusta
-//}
-
-//public interface IIngredient
-//{
-
-//}
-
-//class Bean : IIngredient
-//{ 
-
-//}
 
 public class Ingredient
 {
@@ -34,171 +67,20 @@ public class Ingredient
     public int Amount { get; set; }
 }
 
-class Espresso : IBeverage
+
+abstract class Beverage
 {
-    //private List<Ingredient> Ingredients { get; set; }
-    public string Name { get; set; }
-    public List<Ingredient> Ingredients => throw new System.NotImplementedException();
-    public string CupType { get; set; }
-
-    public IBeverage AddWater(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Water",
-            Amount = amount
-        });
-
-        Water water = new Water()
-        {
-            Amount = 2,
-            Temperature = 90
-        };
-        
-        return this;
-    }
-
-    public IBeverage AddBeans(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Bean",
-            Amount = amount
-        });
-
-
-        return this;
-    }
-
-    public IBeverage AddMilk(int amount)
-    {
-        return this;
-    }
-
+    string Name { get; set; }
+    List<Ingredient> Ingredients { get; }
+    string CupType { get; }
 }
-class Cappucino : IBeverage
-{
-    //private List<Ingredient> Ingredients { get; set; }
-    public string Name { get; set; }
-    public List<Ingredient> Ingredients => throw new System.NotImplementedException();
-    public string CupType { get; set; }
 
-    public IBeverage AddWater(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Water",
-            Amount = amount
-        });
-
-        Water water = new Water()
-        {
-            Amount = 2,
-            Temperature = 90
-        };
-        
-        return this;
-    }
-
-    public IBeverage AddBeans(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Bean",
-            Amount = amount
-        });
-
-
-        return this;
-    }
-
-    public IBeverage AddMilk(int amount)
-    {
-        return this;
-    }
-}
-class Americano : IBeverage
-{
-    //private List<Ingredient> Ingredients { get; set; }
-    public string Name { get; set; }
-    public List<Ingredient> Ingredients => throw new System.NotImplementedException();
-    public string CupType { get; set; }
-
-    public IBeverage AddWater(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Water",
-            Amount = amount
-        });
-
-        Water water = new Water()
-        {
-            Amount = 2,
-            Temperature = 90
-        };
-        
-        return this;
-    }
-
-    public IBeverage AddBeans(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Bean",
-            Amount = amount
-        });
-
-
-        return this;
-    }
-
-    public IBeverage AddMilk(int amount)
-    {
-        return this;
-    }
-}
-class Macchiato : IBeverage
-{
-    //private List<Ingredient> Ingredients { get; set; }
-    public string Name { get; set; }
-    public List<Ingredient> Ingredients => throw new System.NotImplementedException();
-    public string CupType { get; set; }
-
-    public IBeverage AddWater(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Water",
-            Amount = amount
-        });
-
-        Water water = new Water()
-        {
-            Amount = 2,
-            Temperature = 90
-        };
-
-        return this;
-    }
-
-    public IBeverage AddBeans(int amount)
-    {
-        Ingredients.Add(new Ingredient()
-        {
-            Name = "Bean",
-            Amount = amount
-        });
-
-
-        return this;
-    }
-
-    public IBeverage AddMilk(int amount)
-    {
-        return this;
-    }
-}
+class Cappucino : Beverage { }
+class Americano : Beverage { }
+class Espresso : Beverage { }
+class Macchiato : Beverage { }
+class Mocha : Beverage { }
+class Latte : Beverage { }
 
 
 public enum BeanType
